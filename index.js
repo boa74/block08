72160a1da22391242ab988222412613b5ce64237
boa74 <boa8574.kim@gmail.com>
2024-04-15 16:30:29 -0400

    the first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//declare variables in num1, num2, num3 
+let num1 = 7 + 3;
+let num2 = 44 - 4;
+let num3 = 13*3;
+//prompt the message for the vault
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+//prompt the secret code
+alert("the follwing code is" + " " + num1 +"-"+ num2 + "-"+num3);
+
+
+
